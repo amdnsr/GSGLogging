3824aa43b4944b3d113d821b7ef8d637ba0e1f99
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2022-01-04 04:04:47 +0530

    add a boilerplate app

---
+++ main.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from threading import Thread
+from typing import List, Set
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from app.config.configurations import AppConfig, DBConfig
+from app.events.rabbitmq_event_handler import RabbitMQEventHandler
+from app.db.handlers import logs_handler
+
+
+app_config_text = AppConfig.print()
+db_config_text = DBConfig.print()
+
+TITLE = AppConfig.TITLE
+VERSION = AppConfig.VERSION
+DESCRIPTION = AppConfig.DESCRIPTION
+
+HOST = AppConfig.HOST
+PORT = AppConfig.PORT
+RELOAD = AppConfig.RELOAD
+DEBUG = AppConfig.DEBUG
+
+
+app = FastAPI(title=TITLE, version=VERSION,
+              description=DESCRIPTION)
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"],
+                   allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=HOST, port=PORT, reload=RELOAD, debug=DEBUG)
